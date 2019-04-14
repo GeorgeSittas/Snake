db2143a70ed60000ae2b10684ad587d82cedcc9f
Jo <46752250+GeorgeSittas@users.noreply.github.com>
2019-04-14 17:06:35 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
+#include <time.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <ncurses.h>
 
 #include "snake.h"
@@ -8,6 +10,7 @@
 char screen[ROWS][COLUMNS];
 
 int main(void) {
+  srand(time(NULL));
   setup_term(); /* Initializes the curses settings and enters curses mode */
 
   snake_t snake;
